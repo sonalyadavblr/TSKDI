f19435657b1da9a415f3b8bb00a52b0722cde221
Sonal Yadav <sonalyadav.blr@gmail.com>
2020-03-19 01:12:15 +0530

    implemented download page with sample data

--- src/container/Downloads/index.js
+++ src/container/Downloads/index.js
@@ -5,10 +5,34 @@ import './Downloads.css';
 
 export default class Downloads extends React.Component
 {
+	constructor(props) {
+		super(props);
+		this.state = {
+			jsonData: [],
+		}
+	}
+	
+	componentDidMount() {
+		const dataPromise = fetch("http://localhost:5000/downloads/");
+		dataPromise.then((resp) => {
+			const jsonPromise = resp.json();
+			jsonPromise.then((jsonData) => {
+				console.log(jsonData[0].id);
+				this.setState({ jsonData: jsonData });
+			})
+		})
+	}
+
   render()
   {
-    return (
-      <div>Downloads</div>
+	return (
+		<div className="downloads-wrapper">
+				{this.state.jsonData.map((jsonValue) => 
+					<div className="downloads-box" key={jsonValue.id}>{jsonValue.id}{jsonValue.name}</div>
+				)}
+		</div>
     );
   }
 }
+
+
